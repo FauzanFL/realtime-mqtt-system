cba57b255a8f9a8aed6a31d5300082ff08afb106
fauzan.lisan <fauzan.lisan@solusi247.com>
2025-06-20 10:55:53 +0700

    add routerlink active

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   template: `
     <header class="bg-gradient-to-r from-blue-500 to-indigo-700 text-white shadow-lg p-4 sm:px-6 md:px-8 flex items-center justify-between">
       <h1 class="text-2xl sm:text-3xl font-bold tracking-tight">{{title()}}</h1>
@@ -11,17 +11,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
       <nav class="hidden md:flex space-x-2">
         <a
           routerLink="/smarthome"
+          routerLinkActive="font-bold text-yellow-300 bg-indigo-600"
           class="text-white hover:text-yellow-200 hover:bg-indigo-700 px-4 py-2 rounded-lg transition-colors duration-200 text-lg font-semibold">
           Smart Home
         </a>
         <a
           routerLink="/fleet"
+          routerLinkActive="font-bold text-yellow-300 bg-indigo-600"
           class="text-white hover:text-yellow-200 hover:bg-indigo-700 px-4 py-2 rounded-lg transition-colors duration-200 text-lg font-semibold">
           Fleet
         </a>
         <a
           routerLink="/factory"
-          class="text-white hover:text-yellow-200 hover:bg-indigo-700 px-4 py-2 rounded-lg transition-colors duration-200 text-lg font-semibold">
+          routerLinkActive="font-bold text-yellow-300 bg-indigo-600"
+          class="text-white text hover:text-yellow-200 hover:bg-indigo-700 px-4 py-2 rounded-lg transition-colors duration-200 text-lg font-semibold">
           Factory
         </a>
       </nav>
